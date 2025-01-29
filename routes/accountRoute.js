eeaded43b3602228b344c8fae9635fdf287a9b8e
Eric Kodzi <kod24002@byui.edu>
2025-01-29 08:07:39 +0000

    Inserting and validating data

---
+++ routes/accountRoute.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const router = new express.Router()
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+
+// Route to handle account
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+// Registration/Sign-up route
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
+
+module.exports = router;
